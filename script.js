d33df665a3bbdaff001946e37f24e5e63d70e0b9
Brittany Davis <bddavis2011@gmail.com>
2022-07-13 18:15:18 -0500

    halp meeeee!

--- script.js
+++ script.js
@@ -2,14 +2,25 @@ window.addEventListener("load", function() {
     let list = document.getElementById("faultyItems")
     list.style.visibility = "hidden"
     let form = document.querySelector("form")
-    form.addEventListener("submit", (event) => {
+    form.addEventListener("submit", (event) =>{
         event.preventDefault()
         let pilot = document.querySelector('input[name=pilotName]').value
         let copilot = document.querySelector('input[name=copilotName]').value
         let fuelLvl = document.querySelector('input[name=fuelLevel]').value
         let cargoMassKG = document.querySelector('input[name=cargoMass]').value
         formSubmission(document, pilot, copilot, fuelLvl, cargoMassKG)
-    })
+    
+        if (pilot.value == "" || copilot.value == "" || fuelLvl.value == "" || cargoMassKG.value == "") {
+            alert ("All fields are required!")
+        
+        }
+        if (fuelLvl.value == isNAN || cargoMassKG.value == isNAN) {
+            alert ("Make sure to enter valid information for each field!")
+        }
+         
+        
+        
+    });
  
    let listedPlanets;
    let listedPlanetsResponse = myFetch()
@@ -18,6 +29,6 @@ window.addEventListener("load", function() {
    }).then(function () {
        let planet = pickPlanet(listedPlanets)
        addDestinationInfo(planet.name, planet.diameter, planet.star, planet.distance, planet.moons, planet.image)
-   })
+   });
    
 });
